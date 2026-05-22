cedbc9c95376e1906cad3a310d3a039e0ad71577
vboxuser <victorgryca@gmail.com>
2026-05-22 21:40:37 +0000

    feat: adiciona pacote inicial do ROS2

---
+++ src/drawing_turtle/drawing_turtle/caneta.py
@@ -0,0 +1,28 @@
+import rclpy
+from rclpy.node import Node  
+from geometry_msgs.msg import Twist
+
+
+
+class desenhar(Node):
+    def __init__(self):
+        super().__init__('drawing_turtle')
+        self.publisher_= self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
+        timer_period = 0.1
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        msg = Twist()
+        msg.linear.x = 2.0
+        msg.angular.z = 1.0
+        self.publisher_.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TurtleCircle()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
